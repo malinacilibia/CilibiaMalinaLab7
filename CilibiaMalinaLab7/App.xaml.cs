835fc739ddaf75089faa24bf05888ce1fbfb5d0b
Malina <Malina@DESKTOP-2TB49QS>
2024-11-28 20:54:16 +0200

    Laborator 8

--- CilibiaMalinaLab7/App.xaml.cs
+++ CilibiaMalinaLab7/App.xaml.cs
@@ -1,8 +1,27 @@
-﻿namespace CilibiaMalinaLab7
-{
+﻿using System;
+using CilibiaMalinaLab7.Data;
+using System.IO;
+
+namespace CilibiaMalinaLab7 
+{ 
+
     public partial class App : Application
     {
-        public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
         {
             InitializeComponent();
 
---
+++ CilibiaMalinaLab7/ListPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace CilibiaMalinaLab7;
+using System;
+using CilibiaMalinaLab7.Models;
+
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+}
